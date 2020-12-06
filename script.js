b8b2c4ae5d8853445bd0538eb71ce9b01590f2f5
pereysx <75584532+pereysx@users.noreply.github.com>
2020-12-06 22:36:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    // Включаем tooltip у всех элемента
+    $("[data-toggle='tooltip']").tooltip();
+    // Включаем popover у всех элемента
+    $("[data-toggle='popover']").popover();
+});
